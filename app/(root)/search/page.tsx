579f1ac376776816718ee4d25cf2b96a0460f46f
HoangQuangKy <kyhoang487@gmail.com>
2024-01-26 17:47:30 +0700

    add search function and final ui/ux

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { useState } from "react";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-
+import Searchbar from "@/components/shared/SearchBar";
 import { profileTabs } from "@/constants";
 
 import ThreadsTab from "@/components/shared/ThreadsTab";
@@ -10,18 +11,19 @@ import ProfileHeader from "@/components/shared/ProfileHeader";
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 
 import UserCard from "@/components/cards/UserCard";
-async function Page() {
+async function Page({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | undefined };
+}) {
     const user = await currentUser();
     if (!user) return null;
-
-
-
     const userInfo = await fetchUser(user.id);
     if (!userInfo?.onboarded) redirect("/onboarding");
 
     const result = await fetchUsers({
         userId: user.id,
-        searchString: '',
+        searchString: searchParams.q,
         pageNumber: 1,
         pageSize: 25,
         sortBy: 'desc'
@@ -31,7 +33,7 @@ async function Page() {
             <h1 className="head-text mb-10">Search</h1>
 
             {/* Search Bar */}
-
+            <Searchbar routeType='search' />
             <div className="mt-14 flex flex-col gap-9">
                 {result.users.length === 0 ? (
                     <p className="no-result">No Users</p>
