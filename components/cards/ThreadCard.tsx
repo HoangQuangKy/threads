cb16cb88670a9aeb13d79c9cacb3415a364860f4
HoangQuangKy <kyhoang487@gmail.com>
2024-01-27 13:05:24 +0700

    delete Thread

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-
+import DeleteThread from "../form/DeleteThread";
 interface Props {
     id: string;
     currentUserId: string;
@@ -37,6 +37,7 @@ const ThreadCard = ({
     comments,
     isComment,
 }: Props) => {
+
     return (
         <article className={`flex w-full flex-col rounded  ${isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'}`}>
             <div className="flex items-start justify-between">
@@ -62,7 +63,7 @@ const ThreadCard = ({
 
                         <p className="mt-2 text-small-regular text-light-2">{content}</p>
 
-                        <div className={`${isComment && 'mb-10'}`}>
+                        <div className={`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
                             <div className="flex gap-3.5">
                                 <Image
                                     src="/assets/heart-gray.svg"
@@ -86,13 +87,43 @@ const ThreadCard = ({
 
                             {isComment && comments.length > 0 && (
                                 <Link href={`/thread/${id}`}>
-                                    <p className=" mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
+                                    <p className='mt-1 text-subtle-medium text-gray-1'>
+                                        {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+                                    </p>
                                 </Link>
                             )}
                         </div>
                     </div>
                 </div>
+
+                <DeleteThread
+                    threadId={JSON.stringify(id)}
+                    currentUserId={currentUserId}
+                    authorId={author.id}
+                    parentId={parentId}
+                    isComment={isComment}
+                />
             </div>
+            {!isComment && comments.length > 0 && (
+                <div className='ml-1 mt-3 flex items-center gap-2'>
+                    {comments.slice(0, 2).map((comment, index) => (
+                        <Image
+                            key={index}
+                            src={comment.author.image}
+                            alt={`user_${index}`}
+                            width={24}
+                            height={24}
+                            className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
+                        />
+                    ))}
+
+                    <Link href={`/thread/${id}`}>
+                        <p className='mt-1 text-subtle-medium text-gray-1'>
+                            {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+                        </p>
+                    </Link>
+                </div>
+            )}
         </article>
     )
 
